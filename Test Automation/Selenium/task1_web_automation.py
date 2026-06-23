5f5d00081f6f35c87ac98029bf49a2436d24d676
Stefan Henning <hennings.brk@gmail.com>
2026-06-23 14:30:52 +0200

    refactor: replace time.sleep with WebDriverWait in Task 1

--- Test Automation/Selenium/task1_web_automation.py
+++ Test Automation/Selenium/task1_web_automation.py
@@ -1,6 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-import time
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 
 # Setup
 driver = webdriver.Chrome()
@@ -13,10 +15,10 @@ login_button = driver.find_element(By.ID, "login-button")
 username.send_keys("standard_user")
 password.send_keys("secret_sauce")
 login_button.click()
-time.sleep(2)
 
 # Verify Login
-login_verification = driver.find_element(By.CSS_SELECTOR, "[data-test='title']")
+wait = WebDriverWait(driver, 10)
+login_verification = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "[data-test='title']")))
 assert login_verification.text == "Products"
 
 # Verify presence of "Sauce Labs Backpack" on Product Page
